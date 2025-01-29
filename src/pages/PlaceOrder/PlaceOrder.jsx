dbe3edf64419a08c32e8345997a651b4c4da4679
Chetan Gosavi <chetangosavi81@gmail.com>
2025-01-29 12:58:21 +0530

    Food-Delivery

---
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,11 @@
+import "./PlaceOrder.css"
+const PlaceOrder = ()=>{
+    return(
+        <div className="place-order">
+            <h1>PlaceOrder</h1>
+        </div>
+    );
+
+}
+
+export default PlaceOrder;
